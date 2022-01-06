404d4a4d5f613b0ee4f0bb09401dbd5910e53db3
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-06 11:09:45 -0800

    user auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs')
 const {check, validationResult} = require('express-validator');
-
+const jwt = require('jsonwebtoken');
+const config = require('config')
 const User = require('../../models/User')
 
 router.post('/' ,[
@@ -54,6 +55,21 @@ user.password = await bcrypt.hash(password, salt);
 
 await user.save()
 //Return jsonwebtoken
+const payload ={
+    user:{
+        id: user.id
+    }
+
+}
+
+jwt.sign(payload, config.get('jwtSecret'),
+    {expiresIn: 36000},
+    (err, token) => {
+        if(err) throw err;
+        res.json({token});
+    }
+    )
+
 res.send('User route')
 }catch(err){
 console.error(err.message);
