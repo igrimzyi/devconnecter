6c36fa9dbe0f94b486449e2390d4c331c61e177c
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-07 10:25:10 -0800

    get profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -105,6 +105,18 @@ async (req,res)=> {
     res.send('Hello');
 
 
-})
+});
+
+router.get('/', async(req,res) =>{
+        try{
+            const profiles = await Profile.find().populate('user' ,['name', 'avatar'])
+            res.json(profiles);
+        }catch(err) {
+            console.error(err.message);
+            res.status(500).send('server error')
+
+        }
+
+    })
 
 module.exports = router; 
