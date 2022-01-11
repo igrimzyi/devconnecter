0f46378a5231ef8bf8db04cc4c9c748fc8ef7b94
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-10 23:43:28 -0800

    Login User req

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL,LOGIN_SUCCESS } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 export const loadUser = () => async dispatch => {
@@ -50,6 +50,40 @@ try{
 
     dispatch({
         type: REGISTER_FAIL
+    });
+}
+
+
+}
+
+
+export const login = (email,password) => async dispatch => {
+    const config={
+        headers:{ 
+            'Content-Type': 'application/json'
+        }
+    }
+
+const body = JSON.stringify({email,password});
+
+try{
+    const res = await axios.post('api/auth', body, config)
+
+    dispatch({
+    type:LOGIN_SUCCESS,
+    payload: res.data
+}
+
+   ) 
+}catch(err){
+    const errors = err.response.data.errors;
+
+    if(errors){
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+        type: LOGIN_FAIL
     });
 }
 
