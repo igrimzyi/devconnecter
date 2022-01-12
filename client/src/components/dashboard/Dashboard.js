d9c127626c2c42d45967452ac0666453c60294b1
igrimzyi <1isaiahofficial1@gmail.com>
2022-01-11 22:16:16 -0800

    Create Dashboard.js

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Dashboard = props =>{
+    return(
+        <div>
+            Dashboard
+        </div>
+
+    )
+
+
+}
+
+Dashboard.PropTypes ={
+
+    }
+
+
+export default Dashboard
